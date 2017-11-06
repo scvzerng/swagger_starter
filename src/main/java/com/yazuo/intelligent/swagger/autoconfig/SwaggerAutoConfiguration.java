56bffeb4ba8825c8d055cb57918d7ed632555e64
scvzerng <443683059a>
2017-11-06 17:20:54 +0800

    swagger starter 提供更多的默认配置

---
+++ src/main/java/com/yazuo/intelligent/swagger/autoconfig/SwaggerAutoConfiguration.java
@@ -0,0 +1,55 @@
+package com.yazuo.intelligent.swagger.autoconfig;
+
+import io.swagger.annotations.Api;
+import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.Resource;
+
+@Configuration
+@ConditionalOnClass(EnableSwagger2.class)
+@ConditionalOnWebApplication
+@ConditionalOnProperty(SwaggerProperties.PREFIX+".name")
+@EnableConfigurationProperties(SwaggerProperties.class)
+@EnableSwagger2
+public class SwaggerAutoConfiguration {
+    @Resource
+    SwaggerProperties swaggerProperties;
+    @Bean
+    @ConditionalOnMissingBean(Docket.class)
+    public Docket docket(ApiInfo apiInfo){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .forCodeGeneration(true)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ApiInfo.class)
+    public ApiInfo getApiInfo(){
+
+        return new ApiInfoBuilder()
+                .version(swaggerProperties.getVersion())
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .license(swaggerProperties.getLicense())
+                .licenseUrl(swaggerProperties.getLicenseUrl())
+                .contact(new Contact(swaggerProperties.getName(),swaggerProperties.getUrl(),swaggerProperties.getEmail()))
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/yazuo/intelligent/swagger/autoconfig/SwaggerProperties.java
@@ -0,0 +1,106 @@
+package com.yazuo.intelligent.swagger.autoconfig;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = SwaggerProperties.PREFIX)
+public class SwaggerProperties {
+    public static final String PREFIX = "intelligent.swagger";
+
+    /**
+     * API版本
+     */
+    private String version;
+    /**
+     * API标题
+     */
+    private String title;
+    /**
+     * API描述
+     */
+    private String description;
+    /**
+     * 许可证
+     */
+    private String license;
+    /**
+     * 许可证地址
+     */
+    private String licenseUrl;
+    /**
+     * 维护人邮箱
+     */
+    private String email;
+    /**
+     * 维护人姓名
+     */
+    private String name;
+    /**
+     * 维护人网址
+     */
+    private String url;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
